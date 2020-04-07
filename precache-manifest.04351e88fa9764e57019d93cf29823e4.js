4962d7280c44a0ff5cc2bcc3bdd3a94bfa452636
MCUVE <MauricioAntonioMartinez@outlook.com>
2020-04-07 17:49:21 -0500

    Updates

---
+++ precache-manifest.04351e88fa9764e57019d93cf29823e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "524795051087e9f8561129d604049584",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af2c2c0492be6e23818b",
+    "url": "/static/css/main.0f194a55.chunk.css"
+  },
+  {
+    "revision": "6193ae30b8697d157601",
+    "url": "/static/js/2.49143768.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.49143768.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af2c2c0492be6e23818b",
+    "url": "/static/js/main.23c6bc39.chunk.js"
+  },
+  {
+    "revision": "0676fe327a35ace28ded",
+    "url": "/static/js/runtime-main.b129aa53.js"
+  }
+]);
